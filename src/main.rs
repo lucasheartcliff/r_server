e054f7bdf24e0f4403748d6af2db095063557ac6
Lucas Morais <lucascdemorais@gmail.com>
2024-03-27 18:43:42 -0300

    adding full server implementation

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+#![allow(dead_code)]
+
 use server::Server;
+use std::env;
+use website_handler::WebsiteHandler;
 
 mod http;
 mod server;
+mod website_handler;
 
 fn main() {
-    let address = "0.0.0.0:8080".to_string();
-    let server = Server::new(address);
-    server.run();
+    let default_path = format!("{}/public", env!("CARGO_MANIFEST_DIR"));
+    let public_path = env::var("PUBLIC_PATH").unwrap_or(default_path);
+    println!("public path: {}", public_path);
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run(WebsiteHandler::new(public_path));
 }
--- src/server.rs
+++ src/server.rs
@@ -1,22 +1,31 @@
-use crate::http::Request;
-use std::convert;
+use crate::http::{ParseError, Request, Response, StatusCode};
+use std::convert::TryFrom;
 use std::io::Read;
 use std::net::TcpListener;
-use std::thread::Result;
+
+pub trait Handler {
+    fn handle_request(&mut self, request: &Request) -> Response;
+
+    fn handle_bad_request(&mut self, e: &ParseError) -> Response {
+        println!("Failed to parse request: {}", e);
+        Response::new(StatusCode::BadRequest, None)
+    }
+}
+
 pub struct Server {
-    address: String,
+    addr: String,
 }
 
 impl Server {
-    pub fn new(address: String) -> Self {
-        Self { address }
+    pub fn new(addr: String) -> Self {
+        Self { addr }
     }
 
-    pub fn run(self) {
-        println!("Listening on {}", self.address);
-        let listener = TcpListener::bind(&self.address).unwrap();
+    pub fn run(self, mut handler: impl Handler) {
+        println!("Listening on {}", self.addr);
 
-        // while true {}
+        let listener = TcpListener::bind(&self.addr).unwrap();
+
         loop {
             match listener.accept() {
                 Ok((mut stream, _)) => {
@@ -24,22 +33,21 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer));
-                            match Request::try_from(&buffer[..]) {
-                                Ok(request) => {}
-                                Err(e) => println!("Failed to parse request: {}", e),
-                            } // or (&buffer as &[u8])
+
+                            let response = match Request::try_from(&buffer[..]) {
+                                Ok(request) => handler.handle_request(&request),
+                                Err(e) => handler.handle_bad_request(&e),
+                            };
+
+                            if let Err(e) = response.send(&mut stream) {
+                                println!("Failed to send response: {}", e);
+                            }
                         }
-                        Err(e) => println!("Failed to read connection: {}", e),
+                        Err(e) => println!("Failed to read from connection: {}", e),
                     }
                 }
-                Err(e) => println!("Failed to establish connection: {}", e),
+                Err(e) => println!("Failed to establish a connection: {}", e),
             }
-            // let accept = listener.accept();
-            // if accept.is_err() {
-            //     continue;
-            // }
-            //
-            // let (stream, address) = accept.unwrap();
         }
     }
 }
